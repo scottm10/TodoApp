ee4864982da6beb7c7a8734a00cd28d539f8fdf8
Scott Magee <thetruehalfbloodprince@gmail.com>
2016-09-29 15:09:18 -0400

    Remove extraneous call to set itemTextField's delegate

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -24,7 +24,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        itemTextField.delegate = self
         
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Item")
         
